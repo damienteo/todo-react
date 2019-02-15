bf0a25a9f5ca2ed843353a439d8cc46f86a2c045
damienteo <ksteo89@gmail.com>
2019-02-16 00:23:39 +0800

    Further 4 - Edit Task

--- script.jsx
+++ script.jsx
@@ -4,17 +4,21 @@ class List extends React.Component {
     this.inputHandler = this.inputHandler.bind( this );
     this.addHandler = this.addHandler.bind( this );
     this.deleteHandler = this.deleteHandler.bind( this );
+    this.editHandler = this.editHandler.bind( this );
+    this.setEditMode = this.setEditMode.bind( this );
+    this.editInputHandler = this.editInputHandler.bind( this );
   }
 
   state = {
     list : [],
     word : "",
-    validation : ""
+    newWord : "",
+    validation : "",
+    editing : [],
   }
 
   inputHandler(event){
     this.setState({word: event.target.value});
-    console.log("change", event.target.value);
   }
 
   // a button click handler that takes what is in the input, removes it from the input and pushes it into the list.
@@ -23,33 +27,59 @@ class List extends React.Component {
     // Create a length validation on the input. (Ex., must be more than 1 character and less than 200 characters)
     if (word.length >10 && word.length <200) {
       this.setState({word: " ", validation:"", list: list.concat(word)});
-      console.log(list);
     } else {
       this.setState({validation: "Word length should be more than 10 characters."})
-      console.log(this.state.validation);
     }
   }
 
   // Write the code that takes things out of the list.
   deleteHandler(event){
-    // console.log(event.target.value);
     const {list} = this.state;
     var removed = list.splice(event.target.value,1);
-    // console.log("removed",removed);
-    // console.log("list",list);
     this.setState({list: list});
   }
 
+  editInputHandler(event){
+    this.setState({newWord: event.target.value});
+  }
+
+  // Write a todo list item component that edits it's content.
+  editHandler(event){
+    const {list} = this.state;
+    list[event.target.value] = this.state.newWord;
+    this.setState({list: list, newWord: "", editing: []});
+  }
+
+  setEditMode(event){
+    const {editing} = this.state;
+    // console.log("editing", editing)
+    if (!editing.includes(event.target.value)) {
+      if (editing[0]!=undefined) {
+        console.log("trying to splice",event.target.value);
+        let newEditing = editing;
+        newEditing.splice(0, 1, event.target.value);
+        this.setState({editing: newEditing, newWord: ""});
+      } else {
+        this.setState({editing: editing.concat(event.target.value), newWord: ""});
+      }
+    };
+  }
+
   render() {
+    const {word, list, validation, editing} = this.state;
       console.log("rendering");
       return (
         <div className="list">
-          <input onChange={this.inputHandler} value={this.state.word} />
+          <input onChange={this.inputHandler} value={word} />
           <AddItem addButton={this.addHandler} />
-          <ShowValidation validation={this.state.validation} />
+          <ShowValidation validation={validation} />
           <DisplayList 
-            list={this.state.list} 
+            list={list} 
             deleteTask={this.deleteHandler}
+            editTask={this.editHandler}
+            setEditMode={this.setEditMode}
+            editMode={editing}
+            editInputHandler={this.editInputHandler}
           />
         </div>
       );
@@ -76,17 +106,52 @@ class AddItem extends React.Component {
 // write the code that renders the list
 class DisplayList extends React.Component {
 
+  constructor(){
+    super()
+    this.switchEditMode = this.switchEditMode.bind(this);
+    this.showEditInput = this.showEditInput.bind(this);
+    this.showConfirmEdit = this.showConfirmEdit.bind(this);
+  }
+
+  switchEditMode = () => {
+    console.log("switch")
+  }
+
+  showEditInput = (index) => {
+    if (this.props.editMode.includes(index.index.toString())) {
+      return "text";
+    } else {
+      return "hidden";
+    }
+  }
+
+  showConfirmEdit = (index) => {
+
+    const displayNone = {"display":"none"};
+    const displayInline = {"display":"inline"};
+
+    if (this.props.editMode.includes(index.index.toString())) {
+      return displayInline;
+    } else {
+      return displayNone;
+    }
+  }
+
   render() {
+
+    const {deleteTask, setEditMode, editInputHandler, editTask} = this.props;
 
     let ListElements = this.props.list.map ( (item, index) => {
             return(
               <React.Fragment>
                 <li key={index}>{item}</li>
-                <button onClick={this.props.deleteTask} value={index} > Remove this task </button>
+                <button onClick={deleteTask} value={index} > Remove this task </button>
+                <button onClick={setEditMode} value={index} > Edit this task </button>
+                <input type= {this.showEditInput({index})} onChange={editInputHandler}/>
+                <button style={this.showConfirmEdit({index})} onClick={editTask} value={index}>Confirm Edit</button>
               </React.Fragment>
             ) 
           });
-
     return(
       <ol>
         { ListElements }
